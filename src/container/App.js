57c42f22cb0ebaba302148d03a0bef542ef85630
Thiago S Reis Santos <thiago.ufs@gmail.com>
2024-05-16 20:14:45 -0300

    feat: react hook

--- src/container/App.js
+++ src/container/App.js
@@ -1,58 +1,52 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import CardList from "../components/cardList/CardList";
 import SearchBox from '../components/searchBox/SearchBox';
 import Scroll from "../components/scroll/Scroll";
 import ErrorBoundry from "../components/errorBoundry/ErrorBoundry";
 import './App.css';
 
-class App extends Component{
+function App () {
 
-    constructor() {
-        super();
-        this.state = {
-            robots: [],
-            searchField: ''
-        }
-    }
+    const [robots, setRobots] = useState([]);
+    const [searchField, setSearchField] = useState('');
 
-    componentDidMount() {
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
-            .then(users => this.setState({ robots: users}));
+            .then(users => setRobots(users));
+    }, [])
+
+    const onSearchChange = (event) => {
+        setSearchField(event.target.value);
     }
 
-    onSearchChange = (event) => {
-        this.setState({searchField: event.target.value});
-    }
-
-    render() {
-        const { robots, searchField } = this.state;
-        const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchField.toLowerCase());
-        });
+    const filteredRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchField.toLowerCase());
+    });
         
-        return !robots.length ? this.centralPanel(): this.centralPanelCardLis(filteredRobots);
-    }
+    return !robots.length ? 
+                centralPanel(searchField, onSearchChange): 
+                centralPanelCardLis(filteredRobots, searchField, onSearchChange);
+}
 
-    centralPanel() {
-        return <div className="tc">
-                    <h1 className="f1">Robos Amigaveis</h1>
-                    <SearchBox searchField={this.state.searchField} searchChange={this.onSearchChange} />
-                    <h1>Loading!</h1>
-                </div>;
-    }
+function centralPanel(searchField, onSearchChange) {
+    return <div className="tc">
+                <h1 className="f1">Robos Amigaveis</h1>
+                <SearchBox searchField={searchField} searchChange={onSearchChange} />
+                <h1>Loading!</h1>
+            </div>;
+}
 
-    centralPanelCardLis(filteredRobots) {
-        return <div className="tc">
-                    <h1 className="f1">Robos Amigaveis</h1>
-                    <SearchBox searchField={this.state.searchField} searchChange={this.onSearchChange} />
-                    <Scroll>
-                        <ErrorBoundry>
-                            <CardList robots={filteredRobots} />
-                        </ErrorBoundry>
-                    </Scroll>
-                </div>;
-    }
+function centralPanelCardLis(filteredRobots, searchField, onSearchChange) {
+    return <div className="tc">
+                <h1 className="f1">Robos Amigaveis</h1>
+                <SearchBox searchField={searchField} searchChange={onSearchChange} />
+                <Scroll>
+                    <ErrorBoundry>
+                        <CardList robots={filteredRobots} />
+                    </ErrorBoundry>
+                </Scroll>
+            </div>;
 }
     
 export default App;
